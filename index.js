846b849343fa7f7988b0762af638d3715fa27cbe
liaoxuezhi <liaoxuezhi@baidu.com>
2017-09-04 15:13:04 +0800

    init

---
+++ index.js
@@ -0,0 +1,64 @@
+/*
+ * fis
+ * http://fis.baidu.com/
+ */
+
+'use strict';
+
+var postcss = require('postcss');
+
+module.exports = function(content, file, conf) {
+  var plugins = [
+    require('postcss-import-sync'),
+    require('postcss-mixins'),
+    require('postcss-cssnext')
+  ];
+
+  if (conf.report) {
+    plugins.push(require('doiuse')(conf.doiuseOption), require('postcss-reporter'));
+  }
+
+  var ret = postcss(plugins).process(content, conf.sourceMap ? {
+    map: {
+      inline: false
+    },
+    from: file.subpath,
+    to: file.release
+  } : false);
+  content = ret.css;
+  
+  if (ret.map) {
+    var mapping = fis.file.wrap(file.dirname + '/' + file.filename + file.rExt + '.map');
+
+    mapping.setContent(ret.result.map.toString('utf8'));
+
+    // 修改 source 文件名。
+    // var sourceMapObj = JSON.parse(ret.map.toString('utf8'));
+    // sourceMapObj.sources = [file.subpath];
+    // mapping.setContent(JSON.stringify(sourceMapObj, null, 4));
+    
+    var url = mapping.getUrl(fis.compile.settings.hash, fis.compile.settings.domain);
+
+    content = ret.css.replace(/\n?\s*\/\*#\ssourceMappingURL=.*?(?:\n|$)/g, '');
+    content += '\n/*# sourceMappingURL=' +  url + '*/\n';
+
+    file.extras = file.extras || {};
+    file.extras.derived = file.extras.derived || [];
+    file.extras.derived.push(mapping);
+  }
+
+  return content;
+}
+
+
+module.exports.defaultOptions = {
+  doiuseOption: {
+    browsers: [
+      'ie >= 9',
+      '> 1%'
+    ],
+    ignore: ['rem']
+  },
+  sourceMap: false,
+  report: false
+};
